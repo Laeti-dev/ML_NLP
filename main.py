c15a3991ce9fac47efc3aabbbfb9d2f3c2819358
Laetitia Taddei <laetdi@gmail.com>
2023-02-06 07:05:42 +0100

    normalized text

--- main.py
+++ main.py
@@ -13,4 +13,20 @@ print(tweets.head(5))
 # change CROYANCE feature to numeric values yes = 1 no = 0
 tweets['CROYANCE'] = (tweets['CROYANCE'] == 'Yes').astype(int)
 
-print(tweets.head(100))
+print(tweets.head(11))
+
+# NORMALIZE TEXT
+import re
+
+# Create a function
+def normalize(tweet):
+    tweet =re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+    tweet = re.sub('@[^\s]+','USER', tweet)
+    tweet = tweet.lower().replace('ë', 'e')
+    tweet = re.sub('[^a-zA-Z1-9]', ' ', tweet)
+    tweet = re.sub(' +', ' ', tweet)
+    return tweet.strip()
+
+tweets['TWEET'] = tweets['TWEET'].apply(normalize)
+
+print(tweets.head(50))
